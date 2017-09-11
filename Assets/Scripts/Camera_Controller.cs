2da65502d7b4ca40de44d5c8f6eec1ce04f44d52
Kory Davis <ked130@scarletmail.rutgers.edu>
2017-09-10 23:28:12 -0400

    Step 3 - Camera Control (Locked camera)
    Added scripting for the camera which allows for it to update its
    position in correlation to player movement. At the moment, the camera is
    in a locked overhead view of the player/ball.

---
+++ Assets/Scripts/Camera_Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour {
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	//LateUpdate will run after all the items have been processed when the player moves
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
